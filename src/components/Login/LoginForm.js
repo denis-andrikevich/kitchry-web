43acb17527dd8d79e942c1700f25356976414492
dandrikevich <denis.andrikevich@gmail.com>
2017-07-02 01:21:16 +0300

    added styles for login form

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -9,12 +9,14 @@ let LoginForm = props => {
   const { handleSubmit } = props;
   const required = validators.required();
   const email = validators.email('Is not valid email');
-  const style = {
-    margin: 12,
+  const paperStyle = {
+    maxWidth: 500,
+    padding: 25,
+    margin: '15% auto auto'
   };
 
   return (
-    <Paper style={ {padding: 25} }>
+    <Paper style={ paperStyle }>
       <form onSubmit={handleSubmit}>
         <div>
           <Field
@@ -22,6 +24,7 @@ let LoginForm = props => {
             component={TextField}
             floatingLabelText="Email"
             validate={[required, email]}
+            fullWidth={true}
           />
         </div>
         <div>
@@ -30,9 +33,10 @@ let LoginForm = props => {
             component={TextField}
             floatingLabelText="Password"
             validate={required}
+            fullWidth={true}
           />
         </div>
-        <RaisedButton type="submit" label="Submit" primary={true} style={style} />
+        <RaisedButton type="submit" label="Submit" primary={true} style={ { margin: '12px 12px 12px 0'} } />
       </form>
     </Paper>
   )
